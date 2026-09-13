1acf8b8919534e6d1b617394c63645994298fde5
kkb1337 <kkb1337228@gmail.com>
2026-09-13 22:53:13 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_VERSION='1.7.97';
-const CACHE_NAME='ftracker-v1.7.97-clean-architecture';
+const APP_VERSION='1.7.98';
+const CACHE_NAME='ftracker-v1.7.98-clean-architecture';
 const ASSETS=[
 './?v='+APP_VERSION,
 './index.html?v='+APP_VERSION,
