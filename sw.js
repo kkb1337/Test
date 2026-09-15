8ca9dc03fc47f5c7c15568b518661fed2437a1ff
kkb1337 <kkb1337228@gmail.com>
2026-09-15 16:35:39 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const APP_VERSION='1.8.12';
-const CACHE_NAME='ftracker-v1.8.12-goal-editor-ui-v2';
+const APP_VERSION='1.8.13';
+const CACHE_NAME='ftracker-v1.8.13-goal-editor-ui-v2';
 const ASSETS=[
 './?v='+APP_VERSION,
 './index.html?v='+APP_VERSION,
